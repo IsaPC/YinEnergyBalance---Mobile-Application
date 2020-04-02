6a19882e8bbc93c4c4655408ca10a443afdf8554
DucNguyenmp <62087777+DucNguyenmp@users.noreply.github.com>
2020-04-02 23:16:37 +1100

    Add files via upload

--- App.js
+++ App.js
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
-import * as Font from 'expo-font'
-import Home from './screens/home';
-import { AppLoading } from 'expo';
-import Navigator from './routes/drawer';
-
-
-const getFonts = () => Font.loadAsync({
-  'Nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
-  'Nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
-});
-
+import React from 'react';
+import { StyleSheet, Text, View, Image, } from 'react-native';
+import Header from './components/header';
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  if (fontsLoaded) {
-    return (
-      <Navigator />
-    );
-  } else {
-    return (
-      <AppLoading 
-        startAsync={getFonts} 
-        onFinish={() => setFontsLoaded(true)} 
+  return (
+    <View style={styles.container}>
+      <Header
       />
-    )
-  }
-
+      
+      <View style={styles.header}>
+        <Text style={styles.boldText}> YIN YOGA or 
+​UNBRIDLED.EMBODIED.LIVING COACHING </Text>
+        </View>
+        <Image 
+        style={{width:415, flex: 0.89}}
+        source={require ('./assets/yin1.png')}
+      />
+        <View style={styles.body}>
+        <Text> Through an ongoing, gentle, physical practice of Yin Yoga begin to create space in both body and mind to explore self.  
+          This journey is designed for those wanting to combine gentle (non-cardio) physical exercise with timeout to reconnect to your wholeness. </Text>
+        <Text> This journey is designed for those who are ready to be supported to go deep, knowing that it’s time for real change in their lives, 
+          and are willing to invest in their own wellbeing </Text>
+        
+      </View>
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.9,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'white',
+    padding: 10,
+    fontSize: 30,
 
+  },
+  boldText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  
+
+  },
+  
+  
+});
---
+++ components/header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+  return (
+    <View style={styles.header}>
+        <Text style={styles.title}> YIN ENERGY BALANCE </Text>
+        </View>
+        
+      
+  )
+}
+
+const styles = StyleSheet.create({
+    header: {
+      height: 80,
+      backgroundColor: 'lightgreen',
+      flex: 0.11,
+      
+    },
+    title: {
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 34,
+        fontWeight: 'bold',
+        padding: 30,
+    
+        
+    },
+});
