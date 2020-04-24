896920a6e547b622221d60bcd0df54b3f20e9836
DucNguyenmp <62087777+DucNguyenmp@users.noreply.github.com>
2020-04-24 14:53:34 +1000

    Add files via upload

--- App.js
+++ App.js
@@ -1,50 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, } from 'react-native';
-import Header from './components/header';
-export default function App() {
+import { StyleSheet, Text, View,SafeAreaView, ScrollView, } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer} from 'react-navigation';
+import {init} from './Screens/database/db';
+
+import CoachingScreen from './Screens/CoachingScreen'
+import LoginScreen from './Screens/LoginScreen'
+import HomeScreen from './Screens/HomeScreen'
+
+init();
+ class App extends React.Component {
+  
+  render () {
   return (
-    <View style={styles.container}>
-      <Header
-      />
-      
-      <View style={styles.header}>
-        <Text style={styles.boldText}> YIN YOGA or 
-​UNBRIDLED.EMBODIED.LIVING COACHING </Text>
-        </View>
-        <Image 
-        style={{width:415, flex: 0.89}}
-        source={require ('./assets/yin1.png')}
-      />
-        <View style={styles.body}>
-        <Text> Through an ongoing, gentle, physical practice of Yin Yoga begin to create space in both body and mind to explore self.  
-          This journey is designed for those wanting to combine gentle (non-cardio) physical exercise with timeout to reconnect to your wholeness. </Text>
-        <Text> This journey is designed for those who are ready to be supported to go deep, knowing that it’s time for real change in their lives, 
-          and are willing to invest in their own wellbeing </Text>
-        
-      </View>
-    </View>
-  );
+    
+
+<Apps/>
+    
+  
+  )
 }
 
+}
+
+const AppStackNavigator = createStackNavigator ({
+  LoginScreen: {screen : LoginScreen},
+  HomeScreen: {screen: HomeScreen},
+  CoachingScreen: {screen: CoachingScreen},
+})
+const Apps = createAppContainer(AppStackNavigator)
 const styles = StyleSheet.create({
   container: {
-    flex: 0.9, //for android: =1
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: 'white',
-    padding: 10,
-    fontSize: 30,
-
-  },
-  boldText: {
-    fontWeight: 'bold',
-    fontSize: 18,
-  
-
-  },
-  
   
 });
+export default App;
