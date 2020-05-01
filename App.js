45b2133f6ceba11331749246572a30dcbd0f17fe
DucNguyenmp <62087777+DucNguyenmp@users.noreply.github.com>
2020-05-01 12:19:41 +1000

    Add files via upload

--- App.js
+++ App.js
@@ -1,41 +1,4 @@
 import React, {useState, useContext} from 'react';
-
-//import contexts
-import PlandContextProvider from './src/contexts/PlanContext';
-import CalandContextProvider from './src/contexts/CalendarContext';
-import DetailsContextProvider from './src/contexts/DetailsContext';
-// fonts
-import * as Font from 'expo-font';
-
-// routes
-import Routes from './src/routes/routes';
-import { AppLoading } from 'expo';
-
-//SQL
-import { init } from './src/database/db.js';
-
-
-
-/// initialise the database
-init();
-///
-
-/// get the fonts
-const getFonts = () => Font.loadAsync({
-    'Nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
-    'Nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
-  });
-/// 
-
-function App() {
-    const [fontsLoaded, setFontsLoaded] = useState(false);
-
-    if (fontsLoaded)
-        return (
-        /* nest contexts */
-        <DetailsContextProvider>
-            <CalandContextProvider>
-                <PlandContextProvider>import React, {useState, useContext} from 'react';
 import { StyleSheet, Text, View,SafeAreaView, ScrollView, } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer, DrawerItem} from 'react-navigation';
@@ -121,22 +84,5 @@ const styles = StyleSheet.create({
   },
   
 });
-
-export default App;
-                    {/* go to routes */}
-                    <Routes />
-                </PlandContextProvider>
-            </CalandContextProvider>
-        </DetailsContextProvider>
-        );
-    else {
-        return (
-            <AppLoading 
-            startAsync={getFonts} 
-            onFinish={() => setFontsLoaded(true)} 
-            />
-        );
-    }
-} // END main
 
 export default App;
